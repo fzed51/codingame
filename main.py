c4b76964d06c7a6908ef4c433c97b96bbee72d04
Fabien Sanchez <fabien.sanchez@up.coop>
2018-05-30 13:46:48 +0200

    Validé

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ import sys
 import math
 import re
 
+
 def console(message):
     print("Debug messages >> ", message, file=sys.stderr)
 
 
 def binToChuck(binaire):
-    def repl(m):
-        contents = m.group(0)
+    def remplace(m):
+        contents = str(m.group(0))
         if contents[0] == '0':
-            return " 00 ".ljust(len(contents), '0') 
+            return " 00 ".ljust(4+len(contents), '0')
         else:
-            return " 0 ".ljust(len(contents), '0') 
-    return re.sub('(1+)|(0+)', repl, binaire).strip()
+            return " 0 ".ljust(3+len(contents), '0')
+    return re.sub('(1+)|(0+)', remplace, binaire).strip()
 
 
 message = input()
+console("message : {0}".format(message))
 
 binmessage = list(["{:0>7}".format(str(bin(ord(c)))[2:]) for c in message])
+console("binmessage : {0}".format(binmessage))
 
-print(str(binToChuck(''.join(binmessage))))
+output = str(binToChuck(''.join(binmessage)))
+print(output)

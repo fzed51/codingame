54a87e49d254cb2ed93dde51a05284fcd26fc19c
Fabien Sanchez <fabien.sanchez@up.coop>
2018-05-30 12:13:02 +0200

    WIP

--- main.py
+++ main.py
@@ -1,41 +1,23 @@
 import sys
 import math
+import re
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
-
-zonr_d = 0
-zone_f = 0
-zone_a = 0
-
-# the number of points used to draw the surface of Mars.
-surface_n = int(input())
-old_a = -1 old_x
-for i in range(surface_n):
-    # land_x: X coordinate of a surface point. (0 to 6999)
-    # land_y: Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
-    land_x, land_y = [int(j) for j in input().split()]
-    if(old_a == land_y):
-        zone_d = old_x
-        zone_f = land_x
-        zone_a = land_y
-    old_x = land_x
-    old_a = land_y
+def console(message):
+    print("Debug messages >> ", message, file=sys.stderr)
 
 
-print(land_x, land_y, file=sys.stderr)
-# game loop
-while True:
-    # h_speed: the horizontal speed (in m/s), can be negative.
-    # v_speed: the vertical speed (in m/s), can be negative.
-    # fuel: the quantity of remaining fuel in liters.
-    # rotate: the rotation angle in degrees (-90 to 90).
-    # power: the thrust power (0 to 4).
-    x, y, h_speed, v_speed, fuel, rotate, power = [
-        int(i) for i in input().split()]
+def binToChuck(binaire):
+    def repl(m):
+        contents = m.group(0)
+        if contents[0] == '0':
+            return " 00 ".ljust(len(contents), '0') 
+        else:
+            return " 0 ".ljust(len(contents), '0') 
+    return re.sub('(1+)|(0+)', repl, binaire).strip()
 
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr)
 
-    # rotate power. rotate is the desired rotation angle. power is the desired thrust power.
-    print("-20 3")
+message = input()
+
+binmessage = list(["{:0>7}".format(str(bin(ord(c)))[2:]) for c in message])
+
+print(str(binToChuck(''.join(binmessage))))
